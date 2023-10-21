efa754eaeb99d00842bb7aad3e1bb44cf41a2d48
imdevsunny <imdevsunny@gmail.com>
2023-10-21 14:40:24 +0530

    first commit of summary of js datatypes

---
+++ 09summary.js
@@ -0,0 +1,35 @@
+// file includes various data types of javascript
+
+//two types of datatypes broadly primitive and non-primitive (reference datatypes)
+//on the basis of how they are stored in memory and accessed from memory 
+
+//primitive (7 types)
+//strings, Numbers, Boolean, Null, undefined, Symbol, BigInt
+
+const customerName='John Doe'
+
+const customerBalance=12345
+
+const isCustomer=false
+
+const temperature=null
+
+let userEmail;
+
+console.log(userEmail)
+
+const id =Symbol('asdf')
+const anotherId= Symbol('asdf')
+
+const scientificCalculation=123456789345678n //BigInt
+
+//Non-Primitive (reference types)
+// Arrays, Objects, Functions
+
+
+
+
+
+
+
+
